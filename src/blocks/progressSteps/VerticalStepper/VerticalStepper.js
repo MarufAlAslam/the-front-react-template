5ec16f6735bcfb5723ffea8b915c5d93260a9852
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-07 20:00:17 +0600

    updated

--- src/blocks/progressSteps/VerticalStepper/VerticalStepper.js
+++ src/blocks/progressSteps/VerticalStepper/VerticalStepper.js
@@ -1,63 +1,63 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import StepContent from '@mui/material/StepContent';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Container from "../../../components/Container"
+import React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepContent from "@mui/material/StepContent";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Container from "../../../components/Container";
 
 const steps = [
   {
-    label: 'Select campaign settings',
+    label: "Select campaign settings",
     description: `For each ad campaign that you create, you can control how much
               you're willing to spend on clicks and conversions, which networks
               and geographical locations you want your ads to show on, and more.`,
   },
   {
-    label: 'Create an ad group',
+    label: "Create an ad group",
     description:
-      'An ad group contains one or more ads which target a shared set of keywords.',
+      "An ad group contains one or more ads which target a shared set of keywords.",
   },
   {
-    label: 'Create an ad',
+    label: "Create an ad",
     description: `Try out different ad text to see what brings in the most customers,
               and learn how to enhance your ads using features like ad extensions.
               If you run into any problems with your ads, find out how to tell if
               they're running and how to resolve approval issues.`,
   },
   {
-    label: 'Create an ad',
+    label: "Create an ad",
     description: `Try out different ad text to see what brings in the most customers,
               and learn how to enhance your ads using features like ad extensions.
               If you run into any problems with your ads, find out how to tell if
               they're running and how to resolve approval issues.`,
   },
   {
-    label: 'Create an ad',
+    label: "Create an ad",
     description: `Try out different ad text to see what brings in the most customers,
               and learn how to enhance your ads using features like ad extensions.
               If you run into any problems with your ads, find out how to tell if
               they're running and how to resolve approval issues.`,
   },
   {
-    label: 'Create an ad',
+    label: "Create an ad",
     description: `Try out different ad text to see what brings in the most customers,
               and learn how to enhance your ads using features like ad extensions.
               If you run into any problems with your ads, find out how to tell if
               they're running and how to resolve approval issues.`,
   },
   {
-    label: 'Create an ad',
+    label: "Create an ad",
     description: `Try out different ad text to see what brings in the most customers,
               and learn how to enhance your ads using features like ad extensions.
               If you run into any problems with your ads, find out how to tell if
               they're running and how to resolve approval issues.`,
   },
   {
-    label: 'Create an ad',
+    label: "Create an ad",
     description: `Try out different ad text to see what brings in the most customers,
               and learn how to enhance your ads using features like ad extensions.
               If you run into any problems with your ads, find out how to tell if
@@ -81,72 +81,81 @@ const VerticalStepper = () => {
   };
 
   return (
-    <Container maxWidth={800}>
-      <Box>
-        <Stepper activeStep={activeStep} orientation="vertical">
-          {steps.map((step, index) => (
-            <Step
-              key={step.label}
-              sx={{
-                '& .MuiSvgIcon-root': {
-                  width: 32,
-                  height: 32,
-                },
-              }}
-            >
-              <StepLabel
-                optional={
-                  index === 7 ? (
-                    <Typography variant="caption">Last step</Typography>
-                  ) : null
-                }
-                sx={{
-                  '& .MuiStepLabel-label': {
-                    fontWeight: 700,
-                  },
-                }}
-              >
-                {step.label}
-              </StepLabel>
-              <StepContent>
-                <Typography>{step.description}</Typography>
-                <Box sx={{ mb: 2 }}>
-                  <div>
-                    <Button
-                      variant="contained"
-                      onClick={handleNext}
-                      sx={{ mt: 2, mr: 2 }}
+    <div className="py-10">
+      <div className="container">
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-[32px]">
+          <div className="w-full h-full bg-gray-600"></div>
+          <div className="col-span-2">
+            <Box>
+              <Stepper activeStep={activeStep} orientation="vertical">
+                {steps.map((step, index) => (
+                  <Step
+                    key={step.label}
+                    sx={{
+                      "& .MuiSvgIcon-root": {
+                        width: 32,
+                        height: 32,
+                      },
+                    }}
+                  >
+                    <StepLabel
+                      optional={
+                        index === 7 ? (
+                          <Typography variant="caption">Last step</Typography>
+                        ) : null
+                      }
+                      sx={{
+                        "& .MuiStepLabel-label": {
+                          fontWeight: 700,
+                        },
+                      }}
                     >
-                      {index === steps.length - 1 ? 'Finish' : 'Continue'}
-                    </Button>
-                    <Button
-                      variant={'outlined'}
-                      disabled={index === 0}
-                      onClick={handleBack}
-                      sx={{ mt: 2, mr: 1 }}
-                    >
-                      Back
-                    </Button>
-                  </div>
-                </Box>
-              </StepContent>
-            </Step>
-          ))}
-        </Stepper>
-        {activeStep === steps.length && (
-          <Paper square elevation={0} sx={{ p: 3 }}>
-            <Typography>All steps completed - you&apos;re finished</Typography>
-            <Button
-              variant={'outlined'}
-              onClick={handleReset}
-              sx={{ mt: 2, mr: 2 }}
-            >
-              Reset
-            </Button>
-          </Paper>
-        )}
-      </Box>
-    </Container>
+                      {step.label}
+                    </StepLabel>
+                    <StepContent>
+                      <Typography>{step.description}</Typography>
+                      <Box sx={{ mb: 2 }}>
+                        <div>
+                          <Button
+                            variant="contained"
+                            onClick={handleNext}
+                            sx={{ mt: 2, mr: 2 }}
+                          >
+                            {index === steps.length - 1 ? "Finish" : "Continue"}
+                          </Button>
+                          <Button
+                            variant={"outlined"}
+                            disabled={index === 0}
+                            onClick={handleBack}
+                            sx={{ mt: 2, mr: 1 }}
+                          >
+                            Back
+                          </Button>
+                        </div>
+                      </Box>
+                    </StepContent>
+                  </Step>
+                ))}
+              </Stepper>
+              {activeStep === steps.length && (
+                <Paper square elevation={0} sx={{ p: 3 }}>
+                  <Typography>
+                    All steps completed - you&apos;re finished
+                  </Typography>
+                  <Button
+                    variant={"outlined"}
+                    onClick={handleReset}
+                    sx={{ mt: 2, mr: 2 }}
+                  >
+                    Reset
+                  </Button>
+                </Paper>
+              )}
+            </Box>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
