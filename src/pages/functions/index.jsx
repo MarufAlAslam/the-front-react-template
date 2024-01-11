6f155f6a406f669bfe9448a39ffd51ccb7781b8c
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-11 17:32:54 +0600

    branches and functions edited

--- src/pages/functions/index.jsx
+++ src/pages/functions/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "../../components/header";
-import VerticalMinimalDesignedBlogCards from "../../blocks/blog/VerticalMinimalDesignedBlogCards/VerticalMinimalDesignedBlogCards";
+// import VerticalMinimalDesignedBlogCards from "../../blocks/blog/VerticalMinimalDesignedBlogCards/VerticalMinimalDesignedBlogCards";
 import Footer from "../../components/footer";
-import HeroReversed from "../../views/DesktopApp/components/Hero/HeroReversed";
+// import HeroReversed from "../../views/DesktopApp/components/Hero/HeroReversed";
 import { Newsletter } from "../../demos/ecommerce/views/Listing/components";
 
 const Functions = () => {
   return (
     <>
       <Header />
-      <HeroReversed />
-      <VerticalMinimalDesignedBlogCards items={20} />
+      {/* <HeroReversed /> */}
+      {/* <VerticalMinimalDesignedBlogCards items={20} /> */}
       <div className="bg-[#1876D1] py-10">
         <Newsletter />
       </div>
