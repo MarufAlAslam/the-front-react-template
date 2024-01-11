b760685964128bbc5cd25ce1909fecb0a9099371
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-11 18:07:33 +0600

    20 boxes added

--- src/views/PortfolioGrid/components/Main/Main.js
+++ src/views/PortfolioGrid/components/Main/Main.js
@@ -74,6 +74,59 @@ const mock = [
       "On the other hand, we denounce with righteous indignation and dislike",
     title: "Nemo enim ipsam",
   },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img8.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    title: "Lorem ipsum dolor sit amet,",
+  },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img9.jpg",
+    description: "Excepteur sint occaecat cupidatat non proident",
+    title: "Consectetur adipiscing elit",
+  },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img12.jpg",
+    description: "Eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    title: "Labore et dolore magna aliqua",
+  },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img11.jpg",
+    description:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
+    title: "Eiusmod tempor incididunt",
+  },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img11.jpg",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus",
+    title: "Sed ut perspiciatis",
+  },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img12.jpg",
+    description:
+      "Qui blanditiis praesentium voluptatum deleniti atque corrupti",
+    title: "Unde omnis iste natus",
+  },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img9.jpg",
+    description:
+      "On the other hand, we denounce with righteous indignation and dislike",
+    title: "Sit voluptatem",
+  },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img8.jpg",
+    description: "Quos dolores et quas molestias excepturi",
+    title: "Accusantium doloremque",
+  },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img12.jpg",
+    description: "Et harum quidem rerum facilis est et expedita distinctio",
+    title: "Totam rem aperiam",
+  },
+  {
+    image: "https://assets.maccarianagency.com/backgrounds/img11.jpg",
+    description: "Nam libero tempore, cum soluta nobis est eligendi optio",
+    title: "Uae ab illo inventore",
+  },
 ];
 
 const Main = () => {
